43d97639df4185b736631d70d56a2f1c1278fe96
Jose Hernandez <josehernandez@Joses-MacBook-Pro.local>
2024-10-26 16:55:46 -0500

    Initial project configuration

--- src/config.py
+++ src/config.py
@@ -6,28 +6,28 @@
 Module for defining configuration dataclasses to be used with Hydra for managing
 and validating YAML-based configuration files.
 
-This module contains dataclasses that represent the structure of an expected 
-configuration file, which includes settings for various fields relevant to 
-industry sectors, plastic additives, TRI (Toxics Release Inventory) files, 
+This module contains dataclasses that represent the structure of an expected
+configuration file, which includes settings for various fields relevant to
+industry sectors, plastic additives, TRI (Toxics Release Inventory) files,
 and specific columns with associated attributes.
 
 Classes:
     IndustrySector: Represents details about plastic-related industry sectors,
         including a description and NAICS codes.
-    PlasticAdditives: Represents details about plastic additives, including a 
+    PlasticAdditives: Represents details about plastic additives, including a
         description and TRI chemical identifiers.
-    NeededColumn: Defines individual columns expected in TRI files, including 
-        the column name and optional attributes, such as release type, 
-        management type, and flags for specific categories (e.g., 
+    NeededColumn: Defines individual columns expected in TRI files, including
+        the column name and optional attributes, such as release type,
+        management type, and flags for specific categories (e.g.,
         hazardous waste, recycling).
-    FileConfig: Encapsulates a list of NeededColumn entries, defining the 
+    FileConfig: Encapsulates a list of NeededColumn entries, defining the
         required columns for each TRI file.
     TriFiles: Represents all TRI file configurations, specifically organizing
         file_1a, file_1b, file_3a, and file_3c settings.
-    MainConfig: Top-level configuration class aggregating the entire configuration 
+    MainConfig: Top-level configuration class aggregating the entire configuration
         structure, including industry sectors, plastic additives, and TRI files.
 
-This structure allows Hydra to load and manage hierarchical configurations 
+This structure allows Hydra to load and manage hierarchical configurations
 efficiently, enabling clear validation and organized access to configuration data.
 
 """
@@ -40,6 +40,7 @@ from typing import List, Optional
 @dataclass
 class IndustrySector:
     """Industry Sector."""
+
     description: str
     naics_code: List[str] = field(default_factory=list)
 
@@ -47,6 +48,7 @@ class IndustrySector:
 @dataclass
 class PlasticAdditive:
     """Plastic Additives."""
+
     description: str
     tri_chem_id: List[str] = field(default_factory=list)
 
@@ -54,6 +56,7 @@ class PlasticAdditive:
 @dataclass
 class NeededColumn:
     """Needed Column."""
+
     name: str
     relase_type: Optional[str] = None
     management_type: Optional[str] = None
@@ -70,12 +73,14 @@ class NeededColumn:
 @dataclass
 class FileConfig:
     """File Configuration."""
+
     needed_columns: List[NeededColumn] = field(default_factory=list)
 
 
 @dataclass
 class TriFiles:
     """TRI Files."""
+
     file_1a: FileConfig
     file_1b: FileConfig
     file_3a: FileConfig
@@ -85,6 +90,7 @@ class TriFiles:
 @dataclass
 class MainConfig:
     """Main Configuration."""
+
     industry_sectors: IndustrySector
     plastics_additives: PlasticAdditive
     tri_files: TriFiles
--- src/data_processing/utils.py
+++ src/data_processing/utils.py
@@ -1,93 +1,116 @@
-
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python
 
+"""Data processing utils.
+
+Module for utility functions used in data processing tasks, such as loading
+data from text files and generating file paths.
+
+This module contains a class with static
+methods for generating file paths and loading data from text files.
+
+Classes:
+    ConversionFactor: Enum for conversion factors between different units of mass.
+    TriDataHelper: Helper class for working with TRI data files.
+
+"""
+
 import os
-from typing import List
 from enum import Enum
+from typing import List
+
 import pandas as pd
 
 CURRENT_DIRECTORY = os.getcwd()
 
 
 class ConversionFactor(Enum):
-  """Enum for conversion factors between different units of mass.
-  
-  Note:
-    - Assuming TRI wouldn't change the two ways to report the mass.
-    - Pint or other units handler could include if needed in the future,
-      if this would be extended beyond the paper scope to ensure scalability
-      and mantainability.
+    """Enum for conversion factors between different units of mass.
 
-  """
-  POUNDS_TO_KILOGRAMS = 0.453592
-  GRAMS_TO_KILOGRAMS = 10**-3
+    Note:
+      - Assuming TRI wouldn't change the two ways to report the mass.
+      - Pint or other units handler could include if needed in the future,
+        if this would be extended beyond the paper scope to ensure scalability
+        and mantainability.
 
-  @classmethod
-  def from_string(cls, unit: str) -> 'ConversionFactor':
+    """
+
+    POUNDS_TO_KILOGRAMS = 0.453592
+    GRAMS_TO_KILOGRAMS = 10**-3
+
+    @classmethod
+    def from_string(cls, unit: str) -> "ConversionFactor":
         """Get the conversion factor from a string representation of the unit.
-        
+
         Args:
             unit: str: The string representation of the unit.
-        
+
         Returns:
             ConversionFactor: The corresponding conversion factor enum member.
-        
+
         Raises:
             ValueError: If the unit is not recognized.
 
         """
         unit = unit.lower()
-        if unit == 'Pounds':
+        if unit == "Pounds":
             return cls.POUNDS_TO_KILOGRAMS
-        elif unit == 'Grams':
+        elif unit == "Grams":
             return cls.GRAMS_TO_KILOGRAMS
         else:
             raise ValueError(f"Unknown unit: {unit}")
 
 
 class TriDataHelper:
+    """Helper class for working with TRI data files."""
 
-  @classmethod
-  def generate_data_file_path(cls,
-                              file_name: str,
-                              subfolder: str = 'raw',) -> str:
-    """"Generate the file path for the data file.
-    
-    Args:
-      file_name: str: The name of the data file.
-      subfolder: str: The subfolder in which the data file is located.
-      
-    Returns:
-      str: The full path to the data file.
+    @classmethod
+    def generate_data_file_path(
+        cls,
+        file_name: str,
+        subfolder: str = "raw",
+    ) -> str:
+        """Generate the file path for the data file.
 
-    """
-    return os.path.join(CURRENT_DIRECTORY,
-                        'data',
-                        subfolder,
-                        file_name,)
+        Args:
+          file_name: str: The name of the data file.
+          subfolder: str: The subfolder in which the data file is located.
 
-  @classmethod
-  def load_txt_data(cls,
-                    file_path: str,
-                    column_names: List[str],) -> pd.DataFrame:
-    """Load the data from a text file.
+        Returns:
+          str: The full path to the data file.
 
-    Args:
-      file_path: str: The path to the text file.
-      column_names: List[str]: The names of the columns in the data.
-    
-    Returns:
-      pd.DataFrame: The data from the text file.
+        """
+        return os.path.join(
+            CURRENT_DIRECTORY,
+            "data",
+            subfolder,
+            file_name,
+        )
 
-    """
-    df = pd.read_csv(file_path,
-                       sep='\t',
-                       header=None,
-                       encoding='ISO-8859-1',
-                       low_memory=False,
-                       skiprows=[0],
-                       lineterminator='\n',
-                       )
-    df.columns = column_names
-    return df
+    @classmethod
+    def load_txt_data(
+        cls,
+        file_path: str,
+        column_names: List[str],
+    ) -> pd.DataFrame:
+        """Load the data from a text file.
+
+        Args:
+          file_path: str: The path to the text file.
+          column_names: List[str]: The names of the columns in the data.
+
+        Returns:
+          pd.DataFrame: The data from the text file.
+
+        """
+        df = pd.read_csv(
+            file_path,
+            sep="\t",
+            header=None,
+            encoding="ISO-8859-1",
+            low_memory=False,
+            skiprows=[0],
+            lineterminator="\n",
+        )
+        df.columns = column_names
+        return df
